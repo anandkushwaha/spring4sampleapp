7ef1421d64f3d2874867a24b8934cee1d8b4b7b3
anandkushwaha <anandkushwahakiet@gmail.com>
2016-06-12 10:01:53 +0530

    sample hello bean

---
+++ spring4sampleapp/src/main/java/com/anand/bean/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.anand.bean;
+
+public class HelloWorld {
+
+    private String name;
+
+    public String getName() {
+        System.out.println(name);
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- spring4sampleapp/src/main/java/com/anand/java/App.java
+++ spring4sampleapp/src/main/java/com/anand/java/App.java
@@ -1,13 +1,22 @@
 package com.anand.java;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.anand.bean.HelloWorld;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-bean.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+        obj.getName();
+
     }
 }
